8b340a0da4261497f2a55e5da70cee8a42ba6cc4
Jodu555 <Jodu505@gmail.com>
2021-09-04 23:14:46 +0200

    Wrote the basic extraction thing

--- src/test.js
+++ src/test.js
@@ -3,5 +3,9 @@ console.log(decodeDeliveryDate('Dienstag, 7. Sept.'));
 console.log(decodeDeliveryDate('Mittwoch, 8. Sept.'));
 
 function decodeDeliveryDate(deliveryDate) {
+    const day = deliveryDate.split(',')[0].trim();
+    const numday = deliveryDate.split('.')[0].split(',')[1].trim();
+    const month = deliveryDate.split('.')[1].trim();
 
+    console.log('Tag:', day, 'Nummer-Tag:', numday, 'Monat:', month);
 }
