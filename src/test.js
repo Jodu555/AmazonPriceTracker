2f9cec7a5a6debcf5979d8e8df569ff197bd4a09
Jodu555 <Jodu505@gmail.com>
2021-09-04 22:28:50 +0200

    Setuped a test case with 3 vars do destructure the data

---
+++ src/test.js
@@ -0,0 +1,7 @@
+console.log(decodeDeliveryDate('Montag, 6. Sept.'));
+console.log(decodeDeliveryDate('Dienstag, 7. Sept.'));
+console.log(decodeDeliveryDate('Mittwoch, 8. Sept.'));
+
+function decodeDeliveryDate(deliveryDate) {
+
+}
