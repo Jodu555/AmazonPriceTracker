ee65c61e7e3c7c047783b3e174a700ad85d24d86
Jodu555 <Jodu505@gmail.com>
2021-09-05 02:35:30 +0200

    Roughly Implemented the valid Change function! Needs a lot of test cases

--- src/test.js
+++ src/test.js
@@ -1,11 +1,41 @@
-console.log(decodeDeliveryDate('Montag, 6. Sept.'));
-console.log(decodeDeliveryDate('Dienstag, 7. Sept.'));
-console.log(decodeDeliveryDate('Mittwoch, 8. Sept.'));
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+const weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samsatg', 'Sonntag'];
+
+console.log(isValidChange(decodeDeliveryDate('Montag, 6. Sept.'), decodeDeliveryDate('Dienstag, 7. Sept.')));
+// console.log(decodeDeliveryDate('Mittwoch, 8. Sept.'));
+
 
 function decodeDeliveryDate(deliveryDate) {
     const day = deliveryDate.split(',')[0].trim();
     const numday = deliveryDate.split('.')[0].split(',')[1].trim();
     const month = deliveryDate.split('.')[1].trim();
+    return {
+        day,
+        numday,
+        month,
+    }
+}
 
-    console.log('Tag:', day, 'Nummer-Tag:', numday, 'Monat:', month);
+function isValidChange(from, to) {
+    //+ before to cast to Number
+    if (+from.numday + 1 == to.numday) {
+        return true
+    }
+    if (from.numday >= daysInMonth(months.indexOf(from.month + 1))) {
+        if (to.month == getNext(months, months.indexOf(from.month))) {
+            return true
+        }
+    }
+    return false;
+}
+
+function daysInMonth(month) {
+    return new Date(new Date(Date.now()).getFullYear(), month, 0).getDate();
+}
+
+function getNext(arr, index) {
+    while (index + 1 >= arr.length) {
+        index = index - arr.length;
+    }
+    return arr[index + 1];
 }
