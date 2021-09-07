c9248e1a3fb6b0f0e63f193e17ff36881a4719c6
Jodu555 <Jodu505@gmail.com>
2021-09-07 12:37:11 +0200

    Fixed

--- src/routes/fetch/controller.js
+++ src/routes/fetch/controller.js
@@ -107,14 +107,15 @@ function estimateChanges(newest, latest) {
     const changes = [];
     if (JSON.stringify(latest) !== JSON.stringify(newest)) {
         Object.entries(latest).forEach(([key, value]) => {
-            if ((key != 'delivery' && isValidChange(decodeDeliveryDate(newest[key]), decodeDeliveryDate(latest[key]))) &&
-                (key != 'time' && JSON.stringify(newest[key]) != JSON.stringify(value))) {
+            if (key == 'delivery' && !isValidChange(decodeDeliveryDate(newest.delivery), decodeDeliveryDate(latest.delivery))) {
+                return;
+            }
+            if (key != 'time' && JSON.stringify(newest[key]) != JSON.stringify(value))
                 changes.push({
                     key,
                     latest: value,
                     newest: newest[key],
                 });
-            }
         });
     }
 
