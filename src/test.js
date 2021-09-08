942ddb2c027048db992a0aa04289350ad3ae4aa7
Jodu555 <Jodu505@gmail.com>
2021-09-08 12:27:21 +0200

    Added the dashed test cases to test more cases

--- src/test.js
+++ src/test.js
@@ -1,16 +1,36 @@
 
-console.log(decodeDeliveryDate('10. - 13. Sept.'));
-console.log(isValidChange(decodeDeliveryDate('Montag, 6. Sept.'), decodeDeliveryDate('Dienstag, 7. Sept.')));
-console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 17. Sept.')));
-console.log(isValidChange(decodeDeliveryDate('Montag, 30. Sept.'), decodeDeliveryDate('Dienstag, 1. Oct.')));
-console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 16. Oct.')));
+const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+];
+const weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samsatg', 'Sonntag'];
+
+// console.log(decodeDeliveryDate('10. - 13. Sept.'));
+// console.log(isValidChange(decodeDeliveryDate('Montag, 6. Sept.'), decodeDeliveryDate('Dienstag, 7. Sept.')));
+// console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 17. Sept.')));
+// console.log(isValidChange(decodeDeliveryDate('Montag, 30. Sept.'), decodeDeliveryDate('Dienstag, 1. Oct.')));
+// console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 16. Oct.')));
+
+console.log(isValidChange(decodeDeliveryDate('9. - 10. Sept.'), decodeDeliveryDate('10. - 11. Sept.')));
 
 // console.log(decodeDeliveryDate('Mittwoch, 8. Sept.'));
 
 function decodeDeliveryDate(deliveryDate) {
     if (deliveryDate.includes('-'))
         return {
-            break: true,
+            month: deliveryDate.split('-')[1].split('.')[1].trim(),
+            numday: deliveryDate.split('-')[0].split('.')[0].trim(),
+            dashed: true,
         }
     return {
         day: deliveryDate.split(',')[0].trim(),
@@ -20,6 +40,7 @@ function decodeDeliveryDate(deliveryDate) {
 }
 
 function isValidChange(from, to) {
+    console.log(from, to);
     return (
         (+from.numday + 1 == to.numday && from.month == to.month) ||
         (from.numday >= daysInMonth(months.indexOf(from.month + 1)) &&
