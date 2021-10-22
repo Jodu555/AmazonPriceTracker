c9f7072847473416d4b7b9977444cc2cda3c7dc3
Jodu555 <Jodu505@gmail.com>
2021-10-23 01:05:49 +0200

    added the user register schema

--- src/database/schemas.js
+++ src/database/schemas.js
@@ -1,5 +1,11 @@
 const Joi = require('joi');
 
+const userRegisterSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().alphanum().min(5).max(50).required(),
+    email: Joi.string().email().required(),
+});
+
 
 const productCreationSchema = Joi.object({
     amazon_link: Joi.string().min(10).required(),
