330ff5bc394acc0226c693693bda309ec96c892b
Jodu555 <Jodu505@gmail.com>
2022-01-17 16:10:15 +0100

    Tried to test the significant delivery change prop

--- src/test.js
+++ src/test.js
@@ -15,37 +15,50 @@ const months = [
 ];
 const weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samsatg', 'Sonntag'];
 
-// console.log(decodeDeliveryDate('10. - 13. Sept.'));
-// console.log(isValidChange(decodeDeliveryDate('Montag, 6. Sept.'), decodeDeliveryDate('Dienstag, 7. Sept.')));
-// console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 17. Sept.')));
-// console.log(isValidChange(decodeDeliveryDate('Montag, 30. Sept.'), decodeDeliveryDate('Dienstag, 1. Oct.')));
-// console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 16. Oct.')));
+console.log(decodeDeliveryDate('10. - 13. Sept.'));
+console.log(isValidChange(decodeDeliveryDate('Montag, 6. Sept.'), decodeDeliveryDate('Dienstag, 7. Sept.')));
+console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 17. Sept.')));
+console.log(isValidChange(decodeDeliveryDate('Montag, 30. Sept.'), decodeDeliveryDate('Dienstag, 1. Oct.')));
+console.log(isValidChange(decodeDeliveryDate('Montag, 15. Sept.'), decodeDeliveryDate('Dienstag, 16. Oct.')));
 
 console.log(isValidChange(decodeDeliveryDate('9. - 10. Sept.'), decodeDeliveryDate('10. - 11. Sept.')));
+console.log(isValidChange(decodeDeliveryDate('9Nicht verfügbar'), decodeDeliveryDate('10. - 11. Sept.')));
 
 // console.log(decodeDeliveryDate('Mittwoch, 8. Sept.'));
 
 function decodeDeliveryDate(deliveryDate) {
-    if (deliveryDate.includes('-'))
+    if (!deliveryDate)
+        return { break: true }
+    try {
+        if (deliveryDate.includes('Morgen'))
+            return { break: true }
+        if (deliveryDate.includes('-'))
+            return {
+                month: deliveryDate.split('-')[1].split('.')[1].trim(),
+                numday: deliveryDate.split('-')[0].split('.')[0].trim(),
+                dashed: true,
+            }
         return {
-            month: deliveryDate.split('-')[1].split('.')[1].trim(),
-            numday: deliveryDate.split('-')[0].split('.')[0].trim(),
-            dashed: true,
-        }
-    return {
-        day: deliveryDate.split(',')[0].trim(),
-        numday: deliveryDate.split('.')[0].split(',')[1].trim(),
-        month: deliveryDate.split('.')[1].trim(),
-    };
+            day: deliveryDate.split(',')[0].trim(),
+            numday: deliveryDate.split('.')[0].split(',')[1].trim(),
+            month: deliveryDate.split('.')[1].trim(),
+        };
+    } catch (error) {
+        console.error(error);
+        console.log('Decoding Delivery Date failed: ' + deliveryDate);
+    }
 }
 
 function isValidChange(from, to) {
-    console.log(from, to);
-    return (
-        (+from.numday + 1 == to.numday && from.month == to.month) ||
-        (from.numday >= daysInMonth(months.indexOf(from.month + 1)) &&
-            to.month == getNext(months, months.indexOf(from.month)))
-    );
+    if (from.break && to.break)
+        return true;
+    return {
+        value: (
+            (+from.numday + 1 == to.numday && from.month == to.month) ||
+            (from.numday >= daysInMonth(months.indexOf(from.month + 1)) &&
+                to.month == getNext(months, months.indexOf(from.month)))
+        ), significant: false
+    };
 }
 
 function daysInMonth(month) {
